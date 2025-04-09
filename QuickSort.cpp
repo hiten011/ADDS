2b29f2798067c8c77f1dff079886828d6f7cccd3
hiten <hitengupta12345@gmail.com>
2025-04-09 13:19:23 +0930

    h

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,40 +1,32 @@
 #include "QuickSort.h"
 
-vector<int> QuickSort::sort(vector<int> list) { 
-    this->list = list;
+vector<int> QuickSort::sort(vector<int> list) {
+  this->list = list;
 
-    // Recursion
-    // recursion(0, this->list.size());
+  // Recursion
+  // recursion(0, this->list.size());
 
-    // Loop
-    loop();
-    return this->list;
+  // Loop
+  loop();
+  return this->list;
 }
 
 void QuickSort::recursion(int start, int end) {
-   if (start >= end) {
-     return;
-   }
+  if (start >= end) {
+    return;
+  }
 
-   int ptr =  start - 1;
-   int pivot = end - 1;
+  int ptr = helper(end - 1, start - 1, start, end);
 
-   for (int i = start; i < end; i++) {
-       if (this->list[i] <= this->list[pivot]) {
-        ptr++;
-        swap(this->list[ptr], this->list[i]);
-       }
-   }
-
-   recursion(start, ptr);
-   recursion(ptr + 1, end);
+  recursion(start, ptr);
+  recursion(ptr + 1, end);
 }
 
 void QuickSort::loop() {
   queue<vector<int>> q;
 
   // intialising starting
-  vector<int> temp = {0, (int) this->list.size()};
+  vector<int> temp = {0, (int)this->list.size()};
   q.push(temp);
 
   while (!q.empty()) {
@@ -47,17 +39,9 @@ void QuickSort::loop() {
     if (start >= end) {
       continue;
     }
- 
-    int ptr =  start - 1;
-    int pivot = end - 1;
- 
-    for (int i = start; i < end; i++) {
-        if (this->list[i] <= this->list[pivot]) {
-         ptr++;
-         swap(this->list[ptr], this->list[i]);
-        }
-    }
-    
+
+    int ptr = helper(end - 1, start - 1, start, end);
+
     // entring in queue
     vector<int> st = {start, ptr};
     q.push(st);
@@ -65,4 +49,15 @@ void QuickSort::loop() {
     vector<int> en = {ptr + 1, end};
     q.push(en);
   }
+}
+
+int QuickSort::helper(int pivot, int ptr, int start, int end) {
+  for (int i = start; i < end; i++) {
+    if (this->list[i] <= this->list[pivot]) {
+      ptr++;
+      swap(this->list[ptr], this->list[i]);
+    }
+  }
+
+  return ptr;
 }
--- QuickSort.h
+++ QuickSort.h
@@ -9,16 +9,19 @@ using namespace std;
 
 class QuickSort: public Sort {
     public:
-        vector<int> sort(vector<int> list); // TC: O(n^2) on Average: O(nlogn)  SC: log(n)
+        vector<int> sort(vector<int> list); 
 
     private: 
         vector<int> list;
 
         // recursion apprach
-        void recursion(int start, int end);
+        void recursion(int start, int end); // TC: O(n^2) on Average: O(nlogn)  SC: log(n)
 
         // without recursion
         void loop();
+
+        // helper function for Pivot Sort
+        int helper(int pivot, int ptr, int start, int end);
 };
 
 #endif
